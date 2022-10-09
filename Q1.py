7244d5ee37e51e7e6d9a547ffff90e1a7ab4fae2
Yogesh051 <yogeshupadhyay205@gmail.com>
2022-10-10 00:23:16 +0530

    trying

---
+++ Q1.py
@@ -0,0 +1,11 @@
+#take values of length and breadth from user print it is square or rectangle.
+
+
+x = int(input("length:"))
+y = int(input("breadth:"))
+
+if x==y:
+    print("it is a square")
+
+else:
+    print("it is a rectangle")
