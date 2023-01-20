f1a81e9406690c63e79ee3d00c50786020a80a69
Yogesh Upadhyay <114800361+Yogesh051@users.noreply.github.com>
2023-01-20 19:49:35 +0530

    Add files via upload

---
+++ writing ways.py
@@ -0,0 +1,22 @@
+# a=1
+# b=2
+# if a>b:
+#     print("false")
+# else:
+#     print("true")
+
+
+# #another way of writing this
+
+
+# print('false') if a>b else print("true")
+  
+# d=0
+# while(d>0):
+#     print("Happy Birthday Nanu")
+#     d=d-1
+
+d = 1
+while (d>0):
+    print("Happy Birthday Nanu")
+    d = d+1
